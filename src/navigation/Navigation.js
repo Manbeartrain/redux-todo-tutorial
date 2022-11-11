ba73bf5904f23030262fc0c5fa5d5eab685cc9f1
Manbeartrain <ahigareda15@gmail.com>
2022-11-10 17:26:00 -0700

    removed bottom tab navigation

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -5,7 +5,6 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NAVIGATORS } from "../utils/screens";
 
 import LandingScreen from "../screens/LandingScreen";
-import BottomTabNavigation from "./BottomTabNavigation";
 
 const Stack = createStackNavigator();
 
@@ -19,11 +18,6 @@ const Navigation = () => (
 			<Stack.Screen
 				name={NAVIGATORS.LANDING}
 				component={LandingScreen}
-				options={NAVIGATION_OPTIONS}
-			/>
-			<Stack.Screen
-				name={NAVIGATORS.BOTTOM_TAB}
-				component={BottomTabNavigation}
 				options={NAVIGATION_OPTIONS}
 			/>
 		</Stack.Navigator>
